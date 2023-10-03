810b1404d97d8e4b52048c2c83ca7e10d08f1cb7
Bharanidharan05 <bharanimyd@gmail.com>
2023-10-03 22:49:12 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+
+
+export function Home() {
+
+  return (
+    // <h1>Welcome to Home Page⭐</h1>
+    <img id='img-div'  src="https://t4.ftcdn.net/jpg/05/31/67/91/360_F_531679184_3VykZEvx3OvHKnLpl3TdaDYWT1hYjvc9.jpg" alt="Welcome" />
+
+  );
+}
---
+++ src/data.jsx
@@ -0,0 +1,65 @@
+export const data=[
+    {
+        'id':'1',
+        'name':'Bharanidharan',
+        'Gender':'Male',
+        'Role':'Consultant',
+        'YearsOfExperience': '2'
+    },
+    {
+        'id':'2',
+        'name':'Ram',
+        'Gender':'Male',
+        'Role':'Doctor',
+        'YearsOfExperience': '3'
+    },
+    {
+        'id':'3',
+        'name':'James',
+        'Gender':'Female',
+        'Role':'Professor',
+        'YearsOfExperience': '6'
+    },
+    {
+        'id':'4',
+        'name':'Abi',
+        'Gender':'Female',
+        'Role':'Manager',
+        'YearsOfExperience': '3'
+    },
+    {
+        'id':'5',
+        'name':'Maaya',
+        'Gender':'Female',
+        'Role':'Developer',
+        'YearsOfExperience': '7'
+    },
+    {
+        'id':'6',
+        'name':'Dharshni',
+        'Gender':'Female',
+        'Role':'Analyst',
+        'YearsOfExperience': '4'
+    },
+    {
+        'id':'7',
+        'name':'Charan',
+        'Gender':'Male',
+        'Role':'Software Developer',
+        'YearsOfExperience': '2'
+    },
+    {
+        'id':'8',
+        'name':'Suresh',
+        'Gender':'Male',
+        'Role':'engineer',
+        'YearsOfExperience': '6'
+    },
+    {
+        'id':'9',
+        'name':'Sridharan',
+        'Gender':'Male',
+        'Role':'Police',
+        'YearsOfExperience': '7'
+    },
+]
